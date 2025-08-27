192b0e8215fa81a53a92c7421c99ade9752bec01
JWest266 <WestJD@jbu.edu>
2025-08-27 13:52:05 -0500

    Classwork-1

---
+++ ClassWork-1.py
@@ -0,0 +1,11 @@
+student_name = input("Enter Student's Name: ")
+course1 = int(input("Enter the grade for course1: ") )
+course2 = int(input("Enter the grade for course2: ") )
+course3 = int(input("Enter the grade for course3: ") )
+course4 = int(input("Enter the grade for course4: ") )
+course5 = int(input("Enter the grade for course5: ") )
+
+total = course1+course2+course3+course4+course5
+Average = total/5
+print("Your total is:", total)
+print("Your average is:", Average)
