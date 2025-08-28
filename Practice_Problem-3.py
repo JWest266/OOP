e6f8adc72c0e38c38abcaaa872bcb4e09511836f
JWest266 <WestJD@jbu.edu>
2025-08-27 20:07:36 -0500

    Practice Problem 3

---
+++ Practice_Problem-3.py
@@ -0,0 +1,8 @@
+Employee = input("Name of Employee", )
+wages = int(input("wages", ))
+hours = int(input("hours", ))
+days = int(input("days", ))
+
+total_wages = wages*hours*days
+
+print(Employee, total_wages)
